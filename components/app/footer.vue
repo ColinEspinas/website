dd69c9dc19c311e4e55dc767460497d9b876d975
Colin Espinas <colin.espinas@yahoo.fr>
2025-09-19 20:33:38 +0200

    feat: improved footer visuals

--- components/app/footer.vue
+++ components/app/footer.vue
@@ -3,8 +3,8 @@ const { y } = useWindowScroll({ behavior: 'smooth' })
 </script>
 
 <template>
-  <footer class="relative mt-40">
-    <div class="absolute -z-50 bottom-full left-0 w-full h-full border-b border-base-200 mask-radial-at-bottom mask-radial-[50%_50%] mask-radial-from-10% bg-[linear-gradient(to_right,var(--color-base-200)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-base-200)_1px,transparent_1px)] bg-[size:24px_24px]" />
+  <footer class="relative mt-30">
+    <div class="absolute -z-50 bottom-full left-0 w-full h-[240px] border-b border-base-200 mask-radial-at-bottom mask-radial-[50%_50%] mask-radial-from-10% bg-[linear-gradient(to_right,var(--color-base-200)_1px,transparent_1px),linear-gradient(to_bottom,var(--color-base-200)_1px,transparent_1px)] bg-[size:24px_24px]" />
     <div class="flex flex-col max-w-[600px] mx-auto px-10 py-10">
       <nav class="flex justify-between">
         <div class="flex gap-10 font-bold text-base-content-200">
