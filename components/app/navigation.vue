01e026ab09d268c4e4e84d3d0ea5524f93e0a285
Colin Espinas <colin.espinas@yahoo.fr>
2025-09-22 00:58:26 +0200

    refactor: subtle change in nav design

--- components/app/navigation.vue
+++ components/app/navigation.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { useMouseInElement } from '@vueuse/core'
+
+const navRef = useTemplateRef<HTMLDivElement>('nav')
+const { elementX, elementY, elementHeight, elementWidth } = useMouseInElement(navRef)
+
+const gradientPosition = computed(() => {
+  // If mouse is not near the edges, don't show the gradient
+  const padding = 100
+  if (elementX.value > -padding && elementX.value < elementWidth.value + padding
+    && elementY.value > -padding && elementY.value < elementHeight.value + padding) {
+    return `circle at ${elementX.value}px ${elementY.value}px`
+  }
+  return ''
+})
+</script>
+
 <template>
   <nav class="fixed bottom-0 left-0 w-full flex justify-end z-50">
     <div class="absolute -bottom-5 left-0 w-full h-40">
@@ -10,7 +27,11 @@
       <div class="absolute inset-0 backdrop-blur-[12px] h-full w-full bg-transparent mask-[linear-gradient(to_bottom,transparent_75%,black_87.5%,black_100%,transparent_100%)]" />
       <div class="absolute inset-0 backdrop-blur-[24px] h-full w-full bg-transparent mask-[linear-gradient(to_bottom,transparent_87.5%,black_100%,black_100%,transparent_100%)]" />
     </div>
-    <ul class="flex gap-2 mx-auto z-50 bg-base-300/40 backdrop-blur-xl p-2 rounded-full border-1 border-base-200/10 mb-6 shadow-2xl">
+    <ul
+      ref="nav"
+      class="flex gap-2 mx-auto z-50 bg-base-300/40 backdrop-blur-xl p-2 rounded-full border-1 border-base-content-200/5 mb-6 shadow-2xl bg-radial-(--gradient-position) from-base-100/20 to-transparent to-50%"
+      :style="{ '--gradient-position': gradientPosition }"
+    >
       <li>
         <UiButton
           variant="ghost"
